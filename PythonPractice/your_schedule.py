dba27a58f89ce01880b763af9d78c23259ed57c1
JW404 <155033986+MineralSalt69365@users.noreply.github.com>
2025-02-13 17:35:06 -0500

    Add files via upload

---
+++ PythonPractice/your_schedule.py
@@ -0,0 +1,41 @@
+#variables
+course1 = "English"
+teacher1 = "Mrs. Del Buono"
+course2 = "Biology"
+teacher2 = "Mrs. Elaqua"
+course3 = "Function"
+teacher3 = "Mr. Bisogno"
+course4 = "Religion"
+teacher4 = "Mr. Cara"
+course5 = "Music"
+teacher5 = "Mrs. Facchini"
+course6 = "Advanced Function"
+teacher6 = "Mrs. Meleta"
+course7 = "Computer Science"
+teacher7 = "Mr. Gallo"
+course8 = "Chemistry"
+teacher8 = "Ms. Liu"
+
+#Example
+# +------------------------------------------------------------+
+# | 1 |                          English III |       Ms. Lapan |
+# | 2 |                          Precalculus |     Mrs. Gideon |
+# | 3 |                         Music Theory |       Mr. Davis |
+# | 4 |                        Biotechnology |      Ms. Palmer |
+# | 5 |           Principles of Technology I |      Ms. Garcia |
+# | 6 |                             Latin II |    Mrs. Barnett |
+# | 7 |                        AP US History | Ms. Johannessen |
+# | 8 | Business Computer Infomation Systems |       Mr. James |
+# +------------------------------------------------------------+
+
+#codes
+print ("+" + "-"*50 + "+")
+print ("| 1 |" + f"{course1:>25}" + "|" + f"{teacher1:>20}|")
+print ("| 2 |" + f"{course2:>25}" + "|" + f"{teacher2:>20}|")
+print ("| 3 |" + f"{course3:>25}" + "|" + f"{teacher3:>20}|")
+print ("| 4 |" + f"{course4:>25}" + "|" + f"{teacher4:>20}|")
+print ("| 5 |" + f"{course5:>25}" + "|" + f"{teacher5:>20}|")
+print ("| 6 |" + f"{course6:>25}" + "|" + f"{teacher6:>20}|")
+print ("| 7 |" + f"{course7:>25}" + "|" + f"{teacher7:>20}|")
+print ("| 8 |" + f"{course8:>25}" + "|" + f"{teacher8:>20}|")
+print ("+" + "-"*50 + "+")
