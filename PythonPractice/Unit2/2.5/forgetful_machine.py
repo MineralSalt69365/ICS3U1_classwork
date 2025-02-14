b968e414b1147a6f77705ae365af759446f87dfb
JW404 <155033986+MineralSalt69365@users.noreply.github.com>
2025-02-14 13:52:51 -0500

    Add files via upload

---
+++ PythonPractice/Unit2/2.5/forgetful_machine.py
@@ -0,0 +1,8 @@
+input("Give me a word!")
+input("Give me a second word!")
+
+print("")
+
+input("Great, now your favorite number?")
+input("And your second-favorite number...")
+print("Whew!  Wasn't that fun?")
