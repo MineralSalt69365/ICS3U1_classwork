c510a379c6c6478c4724d2ddbe5f73742d7689ec
JW404 <155033986+MineralSalt69365@users.noreply.github.com>
2025-02-14 14:06:30 -0500

    Add files via upload

---
+++ PythonPractice/Unit2/2.5/name_age_salary.py
@@ -0,0 +1,10 @@
+name = input("Hello.  What is your name?")
+age = input(f"Hi, {name}  How old are you?")
+print(f"So you're {age}, eh?  That's not old at all!")
+salary = int(input(f"How much do you make, {name}?"))
+if salary <= 1000:
+    print(f"{salary}! I hope that's per hour and not per year! LOL!")
+else:
+    print("very cool buddy!")
+
+
