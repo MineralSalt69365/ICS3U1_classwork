b614bac6e686ea7db96bb039fd253f407f322345
JW404 <155033986+MineralSalt69365@users.noreply.github.com>
2025-02-21 14:20:23 -0500

    Add files via upload

---
+++ PythonPractice/Unit3/if_elif_else.py
@@ -0,0 +1,22 @@
+team_a_points = 25
+team_a_wins = 15
+
+team_b_points = 20
+team_b_wins = 16
+
+if team_a_points > team_b_points:
+    print("Team A wins!")
+    team_a_wins += 1
+elif team_b_points > team_a_points:
+    print("Team B wins!")
+    team_b_wins += 1
+else:
+    print("Tie.")
+
+if team_a_wins > team_b_wins:
+    print("Team A has more wins than Team B.")
+elif team_b_wins > team_a_wins:
+    print("Team B has more wins than Team A.")
+else:
+    print("Both Teams A and B have the same number of wins.")
+
