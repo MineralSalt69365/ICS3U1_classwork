e2e2cf40ff92f7ebab3afa757583a5a4f0be3f49
JW404 <155033986+MineralSalt69365@users.noreply.github.com>
2025-02-24 12:25:35 -0500

    Update if_elif_else.py

--- PythonPractice/Unit3/if_elif_else.py
+++ PythonPractice/Unit3/if_elif_else.py
@@ -19,4 +19,7 @@ elif team_b_wins > team_a_wins:
     print("Team B has more wins than Team A.")
 else:
     print("Both Teams A and B have the same number of wins.")
-
+    
+#1. That's because the first if statment add 1 to team_a_wins becasue they have more point, now their wins are equal.
+#2. Elif and Else are the condition other than "if", so computer can make different response based on different situation.
+#3. #3. Consider if statement as a paragraph, elif are the sub paragraph within it, if you change elif to if, its like you are taking it out of the paragraph, so it will print our "tie" becasue it is a condition that is other than team_b_points larger than team_a_point
