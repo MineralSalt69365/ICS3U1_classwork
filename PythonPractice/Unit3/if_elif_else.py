8cbbb02edbf6bc92a6e81193040403623807e303
JW404 <155033986+MineralSalt69365@users.noreply.github.com>
2025-02-24 12:25:59 -0500

    Update if_elif_else.py

--- PythonPractice/Unit3/if_elif_else.py
+++ PythonPractice/Unit3/if_elif_else.py
@@ -22,4 +22,4 @@ else:
     
 #1. That's because the first if statment add 1 to team_a_wins becasue they have more point, now their wins are equal.
 #2. Elif and Else are the condition other than "if", so computer can make different response based on different situation.
-#3. #3. Consider if statement as a paragraph, elif are the sub paragraph within it, if you change elif to if, its like you are taking it out of the paragraph, so it will print our "tie" becasue it is a condition that is other than team_b_points larger than team_a_point
+#3. Consider if statement as a paragraph, elif are the sub paragraph within it, if you change elif to if, its like you are taking it out of the paragraph, so it will print our "tie" becasue it is a condition that is other than team_b_points larger than team_a_point
