234754e03ef344889d3dcfc2d5c84f3f8360128a
JW404 <155033986+MineralSalt69365@users.noreply.github.com>
2025-02-25 13:49:57 -0500

    Update how_old_specifically.py

--- PythonPractice/Unit3/3.2/how_old_specifically.py
+++ PythonPractice/Unit3/3.2/how_old_specifically.py
@@ -3,9 +3,9 @@ age = int(input(f"Ok, {name}, how old are you?"))
 
 if age < 16:
     print("you can't drive, you can't vote and you can't rent a car!!")
-elif 16< age <18:
+elif age <18:
     print("you can't vote or rent a car!!")
-elif 18< age < 21:
+elif age < 21:
     print("You can't rent a car!")
 else:
     print("You can do anything legal!!!")    
