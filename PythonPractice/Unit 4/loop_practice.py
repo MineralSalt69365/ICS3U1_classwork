9f11a6f8a33c73752d1a36d147fb15f99377d516
JW404 <155033986+MineralSalt69365@users.noreply.github.com>
2025-03-20 13:19:10 -0400

    Add files via upload

---
+++ PythonPractice/Unit 4/loop_practice.py
@@ -0,0 +1,18 @@
+#1
+count1 = 0
+while count1 <= 100:
+    print(count1)
+    count1 += 1
+
+#2
+count2 = 10
+while count2 <= 30:
+    print(count2)
+    count2 += 3
+
+#3
+player_x = 400
+end_screen = 200
+while player_x >= end_screen:
+    print(f"Player's x position is at {player_x}")
+    player_x -= 3
