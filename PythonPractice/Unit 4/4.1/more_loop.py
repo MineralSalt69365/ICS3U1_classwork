c8f6e04c016896badd6a6badc791ce1c40b5429d
JW404 <155033986+MineralSalt69365@users.noreply.github.com>
2025-03-24 13:24:02 -0400

    Add files via upload

---
+++ PythonPractice/Unit 4/4.1/more_loop.py
@@ -0,0 +1,40 @@
+# 0 to 10 by 1.
+# 10 to 0 by 1.
+# from 491 to 586 by 5.
+# from 124 to 810 by 14.
+# from -233 to -113 by 4.
+# from 215 to 300 by 5.
+# from -310 to -94 by 8.
+# from -476 to -410 by 3.
+# from -327 to -147 by 9.
+# from 484 to 1024 by 18
+
+#1
+count1 = 0
+while count1 <= 10:
+    print(count1)
+    count1 += 1
+
+#3
+count3 = 491
+while count3 <= 586:
+    print(count3)
+    count3 += 5
+
+#5
+count5 = -233
+while count5 <= -113:
+    print(count5)
+    count5 += 1
+
+#7
+count7 = -310
+while count7 < -94:
+    print(count7)
+    count7 += 8
+
+#9
+count9 = -327
+while count9 < -149:
+    print(count9)
+    count9 += 9
