7d1eefa7cf723974bef8f77f667f87584a3d2973
JW404 <155033986+MineralSalt69365@users.noreply.github.com>
2025-03-31 12:47:37 -0400

    Update dice_doubles.py

--- PythonPractice/Unit 4/4.3/dice_doubles.py
+++ PythonPractice/Unit 4/4.3/dice_doubles.py
@@ -2,7 +2,7 @@ import random
 
 dice1 = random.randrange(1,7)
 dice2 = random.randrange(1,7)
-total = dice2 + dice2
+total = dice1 + dice2
 print("HERE COMES THE DICE!")
 print(f"roll 1 : {dice1}")
 print(f"roll 2 : {dice2}")
@@ -12,7 +12,7 @@ while dice1 != dice2:
     print("HERE COMES THE DICE!")
     dice1 = random.randrange(1,7)
     dice2 = random.randrange(1,7)
-    total = dice2 + dice2
+    total = dice1 + dice2
     print(f"roll 1 : {dice1}")
     print(f"roll 2 : {dice2}")
     print(f"The total is {total}!")
