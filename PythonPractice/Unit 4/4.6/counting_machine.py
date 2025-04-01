5ab9a97427cac9c055eaf274dd508ab0e96f9784
JW404 <155033986+MineralSalt69365@users.noreply.github.com>
2025-03-31 20:49:06 -0400

    Add files via upload

---
+++ PythonPractice/Unit 4/4.6/counting_machine.py
@@ -0,0 +1,4 @@
+num = int(input("Count to:"))
+
+for i in range (num+1):
+    print(i, end=" ")
---
+++ PythonPractice/Unit 4/4.6/counting_machine_revisited.py
@@ -0,0 +1,6 @@
+fromm = int(input("Count from:"))
+too = int(input("Count from:"))
+by = int(input("Count from:"))
+
+for i in range(fromm, too+1, by):
+    print(i, end=" ")
